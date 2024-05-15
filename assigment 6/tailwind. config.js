e36a4c8d3e064b01e391d1253c0a851e58d33ed9
Ume Kalsoom <muhammadmoin576@gmail.com>
2024-05-15 21:24:12 +0500

    files added

---
+++ assigment 6/tailwind. config.js
@@ -0,0 +1,33 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>class 12 p-2</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <h1 class="text-3xl font-bold  text-green-700 text-left decoration-red-600 bg-blue-200" >Threek Labiak Pakistan</h1>
+    <div class="flex space-x-4 justify-center bg-yellow-400" >
+        <a href="/index.html" class="bg-blue-700 text-red-300 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+        <a href="https://en.wikipedia.org/wiki/Tehreek-e-Labbaik_Pakistan#Ideology" class="text-red-300 hover:bg-blue-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">ideology</a>
+        <a href="https://www.orfonline.org/research/tehrik-e-labbaik-pakistan-the-new-face-of-barelvi-activism" class="text-red-300 hover:bg-blue-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">activity</a>
+        <a href="https://en.wikipedia.org/wiki/Tehreek-e-Labbaik_Pakistan#References" class="text-red-300 hover:bg-blue-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Services</a>
+      <button class= "text-red-300 py-2  hover:bg-blue-700 hover:text-white justify-end rounded-md px-3"> login</button>
+    </div>
+        <div class="h-80 bg-gradient-to-r from-purple-500 to-gray-500 text-center ">
+            <h2 class=" text-2xl text-yellow-300 font-bold py-10">khadim Hussain rizvi educational & welfare services</h2>
+            <button class= "text-red-300 text-center py-4  hover:bg-pink-700 hover:text-white rounded-md px-2"> Welcome to KHREWS</button>
+        </div>
+    <div class="flex bg-red-200 flex-wrap justify-center h[100vh]">
+        <div class="h-60 w-80 bg-cyan-300 m-5"></div>
+        <div class="h-60 w-80 bg-cyan-300 m-5"></div>
+        <div class="h-60 w-80 bg-cyan-300 m-5"></div>
+        <div class="h-60 w-80 bg-cyan-300 m-5"></div>
+        <div class="h-60 w-80 bg-cyan-300 m-5"></div>
+        <div class="h-60 w-80 bg-cyan-300 m-5"></div>
+        <div class="h-60 w-80 bg-cyan-300 m-5"></div>
+        <div class="h-60 w-80 bg-cyan-300 m-4"></div>
+    </div>
+</body>
+</html>
